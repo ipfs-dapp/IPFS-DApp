acd4cba05000a2f470dc9002240ca69c16038d8c
upendra <upendra861@gmail.com>
2019-09-11 11:40:29 +0530

    Update

--- public/script.js
+++ public/script.js
@@ -2,6 +2,10 @@ $(function() {
   let SubmitButton = $("#Submit-button");
   let ImageForm = $("#ImageForm");
   SubmitButton.click(function(e) {
+    $("#ViewFile").addClass("active");
+    $("#UploadFile").removeClass("active");
+    $("#FileUploadForm").hide();
+    $("#UploadedFiles").show();
     $.fn.serializefiles = function() {
       var obj = $(this);
 
@@ -21,6 +25,7 @@ $(function() {
     };
 
     e.preventDefault();
+    $("#UploadingBufferModal").modal("show");
     $.ajax({
       type: "POST",
       url: "/api/upload",
@@ -38,21 +43,57 @@ $(function() {
 });
 
 function getUploadedfile(Hash) {
-  let url = `https://ipfs.infura.io/ipfs/${Hash}`;
-  document.getElementById("url").innerHTML = url;
-  document.getElementById("url").href = url;
-  document.getElementById("output").src = url;
-  document.getElementById("Submit-button").value = "Update this image";
+  $("#UploadingBufferModal").modal("hide");
+  let Title = $("#TitleOfFile").val();
+  let Description = $("#DescriptionOfFile").val();
+  $("#ImageContainer").append(`
+  <div class="card mb-2 Product">
+              <div class="row no-gutters">
+                <div class="col-md-4 image-block">
+                  <img id="output" src="https://ipfs.infura.io/ipfs/${Hash}" class="card-img" />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body" style="font-size:25px;">
+                    <div style="font-size:25px;">
+                      <b>Title :</b> ${Title}
+                    </div>
+                    <br>
+                    <div style="font-size:25px;">
+                      <b>Description : </b> ${Description}
+                    </div>
+                    <br>
+                    <div style="font-size:20px;">
+                    <a id="url" href="https://ipfs.infura.io/ipfs/${Hash}">https://ipfs.infura.io/ipfs/${Hash}</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+  `);
 }
 
+// $(function() {
+//   $.get("/api/getUploadedfile", Hash => {
+//     if (Hash) {
+//       let url = `https://ipfs.infura.io/ipfs/${Hash}`;
+//       document.getElementById("url").innerHTML = url;
+//       document.getElementById("url").href = url;
+//       document.getElementById("output").src = url;
+//     }
+//   });
+// });
+
 $(function() {
-  $.get("/api/getUploadedfile", Hash => {
-    if (Hash) {
-      let url = `https://ipfs.infura.io/ipfs/${Hash}`;
-      document.getElementById("url").innerHTML = url;
-      document.getElementById("url").href = url;
-      document.getElementById("output").src = url;
-      document.getElementById("Submit-button").value = "Update this image";
-    }
+  $("#UploadFile").click(function() {
+    $("#ViewFile").removeClass("active");
+    $("#UploadFile").addClass("active");
+    $("#UploadedFiles").hide();
+    $("#FileUploadForm").show();
+  });
+  $("#ViewFile").click(function() {
+    $("#ViewFile").addClass("active");
+    $("#UploadFile").removeClass("active");
+    $("#FileUploadForm").hide();
+    $("#UploadedFiles").show();
   });
 });
